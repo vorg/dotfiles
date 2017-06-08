3458241984a1337d4de7671c68de067fc83ae3d6
Marcin Ignac <marcin.ignac@gmail.com>
2017-06-08 12:06:38 +0100

    Add backup.sh

---
+++ backup.sh
@@ -0,0 +1,6 @@
+rsync --delete --info=progress2 -avze ssh ~/Workspace rsync@192.168.1.137::Storage/Bak/
+rsync --delete --info=progress2 -avze ssh ~/WorkspaceSort rsync@192.168.1.137::Storage/Bak/
+rsync --delete --info=progress2 -avze ssh ~/WorkspaceArchive rsync@192.168.1.137::Storage/Bak/
+rsync --delete --info=progress2 -avze ssh ~/WorkspaceTodo rsync@192.168.1.137::Storage/Bak/
+rsync --delete --info=progress2 -avze ssh ~/Desktop rsync@192.168.1.137::Storage/Bak/
+rsync --delete --info=progress2 -avze ssh ~/Dropbox rsync@192.168.1.137::Storage/Bak/
